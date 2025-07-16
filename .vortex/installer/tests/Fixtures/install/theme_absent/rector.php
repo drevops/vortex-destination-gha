b30bb801cd6e667f48059e88377f0a329be90aec
Vortex deployer <deploy+vortex-gha@drevops.com>
2025-07-16 10:59:26 +0000

    Deployment commit

--- .vortex/installer/tests/Fixtures/install/theme_absent/rector.php
+++
@@ -1,8 +0,0 @@
-@@ -42,7 +42,6 @@
- 
-   $rectorConfig->paths([
-     $drupalRoot . '/modules/custom',
--    $drupalRoot . '/themes/custom',
-     $drupalRoot . '/sites/default/settings.php',
-     $drupalRoot . '/sites/default/includes',
-     __DIR__ . '/tests',

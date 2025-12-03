28c79f219d6edba30a6f3b6857f17af77b37bafa
Vortex deployer <deploy+vortex-gha@drevops.com>
2025-12-03 11:54:30 +0000

    Deployment commit

--- .vortex/installer/tests/Fixtures/handler_process/tools_groups_no_be_lint_circleci/web/themes/custom/star_wars/tests/src/Functional/ExampleTest.php
+++
@@ -1,8 +0,0 @@
-@@ -19,7 +19,6 @@
-   /**
-    * {@inheritdoc}
-    *
--   * @phpcs:disable Generic.CodeAnalysis.UselessOverridingMethod.Found
-    */
-   #[\Override]
-   protected function setUp(): void {

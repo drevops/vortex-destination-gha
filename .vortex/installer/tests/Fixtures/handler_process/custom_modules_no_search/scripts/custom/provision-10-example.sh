bc13b90be0645c0c9ac8a4f2ed9cef3e93b5664a
Vortex deployer <deploy+vortex-gha@drevops.com>
2026-04-02 06:58:16 +0000

    Deployment commit

--- .vortex/installer/tests/Fixtures/handler_process/custom_modules_no_search/scripts/custom/provision-10-example.sh
+++
@@ -1,9 +0,0 @@
-@@ -57,8 +57,6 @@
-   task "Installing custom site modules."
-   drush pm:install sw_base
- 
--  drush pm:install sw_search
--
-   drush pm:install sw_demo
- 
-   task "Running deployment hooks."

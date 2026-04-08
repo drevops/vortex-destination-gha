2a348aa9e9c5652664ae066d27c21394819bbfb8
Vortex deployer <deploy+vortex-gha@drevops.com>
2026-04-08 00:47:41 +0000

    Deployment commit

---
+++ web/modules/contrib/generated_content/modules/generated_content_example2/src/GeneratedContentExample2Helper.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\generated_content_example2;
+
+use Drupal\generated_content\Helpers\GeneratedContentHelper;
+
+/**
+ * Class GeneratedContentExample2Helper.
+ *
+ * Example of content generation helper class extension.
+ *
+ * @package \Drupal\generated_content_example2
+ */
+class GeneratedContentExample2Helper extends GeneratedContentHelper {
+
+  /**
+   * Random tags.
+   *
+   * @param int|null $count
+   *   Number of term.
+   *
+   * @return \Drupal\taxonomy\TermInterface[]
+   *   Terms.
+   */
+  public static function randomTags(?int $count = NULL): array {
+    return static::randomTerms('tags', $count);
+  }
+
+}

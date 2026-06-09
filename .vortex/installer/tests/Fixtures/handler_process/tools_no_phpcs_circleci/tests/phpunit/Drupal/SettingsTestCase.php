af8fde055581e57799b64c726d2f01cef792fb41
Vortex deployer <deploy+vortex-gha@drevops.com>
2026-06-09 04:39:17 +0000

    Deployment commit

--- .vortex/installer/tests/Fixtures/handler_process/tools_no_phpcs_circleci/tests/phpunit/Drupal/SettingsTestCase.php
+++
@@ -1,8 +0,0 @@
-@@ -11,7 +11,6 @@
-  *
-  * Base class for testing Drupal settings.
-  *
-- *  phpcs:disable Drupal.NamingConventions.ValidVariableName.LowerCamelName
-  */
- abstract class SettingsTestCase extends TestCase {
- 

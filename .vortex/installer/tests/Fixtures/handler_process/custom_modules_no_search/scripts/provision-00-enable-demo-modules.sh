31d827dabc3cf5d35e3aae4a9d93b2298d86ce03
Vortex deployer <deploy+vortex-gha@drevops.com>
2026-07-29 05:30:01 +0000

    Deployment commit

--- .vortex/installer/tests/Fixtures/handler_process/custom_modules_no_search/scripts/provision-00-enable-demo-modules.sh
+++
@@ -1,9 +0,0 @@
-@@ -84,8 +84,6 @@
- task "Installing custom site modules."
- drush pm:install sw_base
- 
--drush pm:install sw_search
--
- drush pm:install sw_demo
- pass "Installed custom site modules."
- 

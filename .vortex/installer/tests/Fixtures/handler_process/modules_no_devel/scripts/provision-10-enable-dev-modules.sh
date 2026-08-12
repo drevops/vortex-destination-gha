57d37caa7b7fd6b6ac1e12efba009f8f0469288a
Vortex deployer <deploy+vortex-gha@drevops.com>
2026-08-12 00:10:43 +0000

    Deployment commit

--- .vortex/installer/tests/Fixtures/handler_process/modules_no_devel/scripts/provision-10-enable-dev-modules.sh
+++
@@ -1,9 +0,0 @@
-@@ -38,8 +38,4 @@
- drush pm:install sdc_devel || true
- pass "Installed Single Directory Component development tools."
- 
--task "Installing Devel module."
--drush pm:install devel || true
--pass "Installed Devel module."
--
- info "Finished development modules operations."

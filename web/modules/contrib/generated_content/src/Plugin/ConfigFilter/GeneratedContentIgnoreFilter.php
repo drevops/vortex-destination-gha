429bbc12b82eab7737e1d2a563ff848a9770991c
Vortex deployer <deploy+vortex-gha@drevops.com>
2026-08-20 23:14:25 +0000

    Deployment commit

---
+++ web/modules/contrib/generated_content/src/Plugin/ConfigFilter/GeneratedContentIgnoreFilter.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\generated_content\Plugin\ConfigFilter;
+
+use Drupal\config_filter\Plugin\ConfigFilterBase;
+
+/**
+ * Ignore all generated_content config.
+ *
+ * @ConfigFilter(
+ *   id = "generated_content_config_ignore",
+ *   label = "Generated Content Config Ignore",
+ *   weight = 100
+ * )
+ */
+class GeneratedContentIgnoreFilter extends ConfigFilterBase {
+
+  /**
+   * {@inheritdoc}
+   *
+   * @phpstan-ignore-next-line
+   */
+  public function filterWrite($name, array $data): array {
+    if ($name === 'core.extension') {
+      $excluded_modules = ['generated_content' => 'generated_content'];
+      $data['module'] = array_diff_key($data['module'], $excluded_modules);
+    }
+
+    return $data;
+  }
+
+}

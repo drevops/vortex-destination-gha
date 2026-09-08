5b94017caf784b7ac3bf741a03c83fa7ffb14fb7
Vortex deployer <deploy+vortex-gha@drevops.com>
2026-09-08 10:11:04 +0000

    Deployment commit

---
+++ web/modules/contrib/generated_content/modules/generated_content_example2/src/GeneratedContentExample2ServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\generated_content_example2;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\DependencyInjection\ServiceProviderBase;
+
+/**
+ * Class GeneratedContentExample2ServiceProvider.
+ *
+ * Example of asset generator service replacement with an extending class.
+ *
+ * @package Drupal\generated_content_example2
+ */
+class GeneratedContentExample2ServiceProvider extends ServiceProviderBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alter(ContainerBuilder $container): void {
+    if ($container->hasDefinition('generated_content.asset_generator')) {
+      $definition = $container->getDefinition('generated_content.asset_generator');
+      $definition->setClass('Drupal\generated_content_example2\GeneratedContentExample2AssetGenerator');
+    }
+  }
+
+}

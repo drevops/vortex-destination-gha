19b8bf870e2b5fab061686d431d124a1ee41dd5c
Vortex deployer <deploy+vortex-gha@drevops.com>
2026-09-21 09:28:31 +0000

    Deployment commit

--- .vortex/installer/tests/Fixtures/handler_process/tools_no_phpstan/web/sites/default/includes/modules/settings.fast_404.php
+++
@@ -1,7 +0,0 @@
-@@ -55,6 +55,5 @@
-   // the module passes an array where an exception is expected. Patch it with
-   // https://www.drupal.org/files/issues/2023-08-24/fast_404_3x-3194034-10.patch
-   // @see https://www.drupal.org/project/fast_404/issues/3194034
--  // @phpstan-ignore-next-line
-   fast404_preboot($settings);
- }
